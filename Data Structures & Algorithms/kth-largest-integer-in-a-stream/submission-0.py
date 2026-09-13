b0f71a2498da4fe951abcc14e8f5d8fb0cb73b7c
VANSH VERMA <146957780+iam-vsr@users.noreply.github.com>
2026-09-13 13:05:05 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.py
@@ -0,0 +1,20 @@
+# A min-heap always keeps the smallest value at the top.
+# If the heap contains the k largest elements,
+# then the smallest among them is exactly the k-th largest overall.
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k=k
+        self.min_heap=nums
+        heapq.heapify(self.min_heap)
+
+        while len(self.min_heap)>self.k:
+            heapq.heappop(self.min_heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.min_heap, val)
+
+        if len(self.min_heap)>self.k:
+            heapq.heappop(self.min_heap)
+        
+        return self.min_heap[0]
