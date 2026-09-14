6afb13cbecd17ac1c2c6aeb26112680b5013d5c1
VANSH VERMA <146957780+iam-vsr@users.noreply.github.com>
2026-09-14 19:19:00 +0530

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        queue=deque()
+        rows=len(grid)
+        cols=len(grid[0])
+        fresh=0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]==1:
+                    fresh+=1
+                if grid[i][j]==2:
+                    queue.append((i,j))
+        
+        t=0
+        dirs=[[1,0],[-1,0],[0,1],[0,-1]]
+
+        while fresh>0 and queue:
+            #level is important here
+            # without for loop, every single orange u pop = 1 minute.
+            for _ in range(len(queue)): 
+                r,c=queue.popleft()
+                
+                for dr,dc in dirs:
+                    nr=r+dr
+                    nc=c+dc
+                    
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
+                        fresh-=1
+                        grid[nr][nc]=2
+                        queue.append((nr,nc))
+            t+=1 #increment only after 1 level is completed
+        
+        return t if fresh==0 else -1
+
